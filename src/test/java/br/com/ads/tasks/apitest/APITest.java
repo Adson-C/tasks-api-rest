0a3c996096cf16238ee4facfcfd1e86aca6ba42b
Adson Conceição <adsonconcecao@yahoo.com.br>
2021-12-27 23:05:33 -0300

    modificado data

--- src/test/java/br/com/ads/tasks/apitest/APITest.java
+++ src/test/java/br/com/ads/tasks/apitest/APITest.java
@@ -29,7 +29,7 @@ public class APITest {
 	public void deveAdicionarTarefasComSucesso() {
 		
 		RestAssured.given()
-			.body("{ \"task\": \"Teste via API\", \"dueDate\": \"2021-12-25\" }")
+			.body("{ \"task\": \"Teste via API\", \"dueDate\": \"2021-12-26\" }")
 			.contentType(ContentType.JSON)
 		.when()
 			.post("/todo")
